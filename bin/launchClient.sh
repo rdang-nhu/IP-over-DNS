0d6d9f6f642858350e1af7222500e1e611ccb1a8
rdang-nhu <raphael.dang-nhu@polytechnique.edu>
2017-09-14 19:37:51 +0200

    Ajout de launchClient.sh

---
+++ bin/launchClient.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Indique au système que l'argument qui suit est le programme utilisé pour exécuter ce fichier
+# En règle générale, les "#" servent à mettre en commentaire le texte qui suit comme ici
+
+java -cp "/home/raphael/Documents/Modal/Projet_Reseaux_Test/lib/*:." -Djava.library.path=/home/raphael/Documents/Modal/Projet_Reseaux_Test/bin DNS_Client
+
+
+exit 0
